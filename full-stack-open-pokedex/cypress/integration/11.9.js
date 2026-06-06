6d2300924cc48f04f8f073580e3292aad62e112e
laptop <juha.jylhakoski@gmail.com>
2026-06-06 09:23:08 +0300

    merge repositories

---
+++ full-stack-open-pokedex/cypress/integration/11.9.js
@@ -0,0 +1,16 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    //cy.visit('http://localhost:8081')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
+
+describe('Pokemon', function() {
+  it('pokemon page can be opened', function() {
+    cy.visit('http://localhost:5000', { timeout: 45000 })
+    cy.get('a[href*="/pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
+  })
+})
